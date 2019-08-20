83c99b4e836593947f7ee51c6a2bcbbd4727d47f
Lisham Singh <Lisham.Singh@nelnet.net>
2019-08-20 11:12:22 -0500

    August 2019 Small Updates
    -Unhides attachments for PRO members in the STIC group
    -Adds Blue Button Template CSS
    -Adds Blue Rows Table CSS
    -Adds editor-config.js to track Editor configuration

---
+++ js/editor-config.js
@@ -0,0 +1,28 @@
+CKEDITOR.editorConfig = function( config )
+{
+	config.toolbar =
+		[
+			['MindTouchSave','MindTouchCancel'],
+			['ViewMenu'],
+			['WhoIsEditing'],
+			['Cut','Copy','Paste','PasteText','PasteFromWord'],
+			['Transformations'],
+			['Undo','Redo','-','Find','Replace','-','SelectAll'],
+			['HorizontalRule','SpecialChar','PageBreak','Transclusion','LearningPath'],
+			'/',
+			['Font','FontSize'],
+			['TextColor','BGColor'],
+			['Bold','Italic','Underline','Strike','-','Subscript','Superscript','Code','PlainText','RemoveFormat'],
+			['NumberedList','BulletedList','DefinitionList','-','Outdent','Indent'],
+			['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+			'/',
+			['Normal','H3','H4','H5','StylesMenu'],
+			['MindTouchLink','Unlink','Anchor','MindTouchTable','MindTouchImage','Video','MindTouchTemplates']
+		];
+};
+
+CKEDITOR.editorConfig = function( config ){
+config.stylesSet.push({ name : 'Internal Users', element : 'div', wrap:true, attributes : { 'class' : 'mt-style-conditional', 'if' : 'user.groups["NBS Viewer"] || user.groups["PRO Author"] || user.groups["PRO Editor"] || user.groups["PRO Admin"]'} });
+config.stylesSet.push({ name : 'HE and K12 School Users', element : 'div', wrap:true, attributes : { 'class' : 'mt-style-conditional', 'if' : 'user.groups["Nelnet Campus Commerce Schools"] ||  user.groups["K12 FACTS Schools"]'} });
+config.stylesSet.push({name: 'Blue Rows Table', element: 'table', attributes: { 'class': 'blue-rows' }});
+}
